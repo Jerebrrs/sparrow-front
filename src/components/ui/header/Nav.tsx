f053f09233076569dfccdb1c0921a9bf710a8cbe
kevin <kevinbrrs96@gmail.com>
2024-11-18 12:22:32 -0300

    Feat Navbar/NavbarMobile

--- src/components/ui/header/Nav.tsx
+++ src/components/ui/header/Nav.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import SelectButton from './SelectButton';
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarSub, MenubarSubContent, MenubarSubTrigger, MenubarTrigger } from '../menubar';
 
 interface NavProps {
   containerStyles?: string;
@@ -20,16 +21,16 @@ const Nav: React.FC<NavProps> = ({ containerStyles = '', linkStyles = '', underL
   const path = usePathname();
 
   return (
-    <nav className={`${containerStyles}` }>
-      {links.map((link, index) => (
-        <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>
+    <nav className={`${containerStyles}`}>
+      {/* {links.map((link, index) => (
+        <Link href={link.path} key={index} classNameName={`capitalize ${linkStyles}`}>
           {link.path === path && (
             <motion.span
               initial={{ y: '-100%' }}
               animate={{ y: 0 }}
               transition={{ type: 'tween' }}
               layoutId='underline'
-              className={`${underLineStyles}`}
+              classNameName={`${underLineStyles}`}
             />
           )}
           {link.name}
@@ -37,8 +38,31 @@ const Nav: React.FC<NavProps> = ({ containerStyles = '', linkStyles = '', underL
 
         </Link>
 
-      ))}
-      <SelectButton />
+      ))} */}
+
+
+      {/* <div className="flex lg:hidden">
+        <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <span className="sr-only">Open main menu</span>
+          <svg className="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path strokeLinecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div> */}
+      <div className="hidden lg:flex lg:gap-x-12">
+
+        <SelectButton />
+
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Informacion</a>
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Blog</a>
+        {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">Company</a> */}
+      </div>
+      {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" className="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+      </div> */}
+
+
+
     </nav>
   );
 };
--- src/components/ui/header/SelectButton.tsx
+++ src/components/ui/header/SelectButton.tsx
@@ -9,11 +9,11 @@ import {
 } from '@heroicons/react/24/outline'
 
 const solutions = [
-    { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-    { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-    { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
-    { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-    { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+    { name: 'Jeans', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+    { name: 'Remeras', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+    { name: 'Gorras', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
+    { name: 'Buzos', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+    { name: 'Zapatillas', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
 ]
 const callsToAction = [
     { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
@@ -23,46 +23,32 @@ const callsToAction = [
 export default function SelectButton() {
     return (
         <Popover className="relative">
-            <PopoverButton className="inline-flex items-center text-2xl font-semibold text-gray-900">
-                <span>Solutions</span>
-                <ChevronDownIcon aria-hidden="true" className="size-5" />
-            </PopoverButton>
-
-            <PopoverPanel
-                transition
-                className="absolute bg-red-300 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-                <div className="w-screen bg-green-300 max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 shadow-lg ring-1 ring-gray-900/5">
-                    <div className="p-4">
-                        {solutions.map((item) => (
-                            <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-                                <div className="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                    <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
-                                </div>
-                                <div>
-                                    <a href={item.href} className="font-semibold text-gray-900">
-                                        {item.name}
-                                        <span className="absolute inset-0" />
-                                    </a>
-                                    <p className="mt-1 text-gray-600">{item.description}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                        {callsToAction.map((item) => (
-                            <a
-                                key={item.name}
-                                href={item.href}
-                                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
-                            >
-                                <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                                {item.name}
-                            </a>
-                        ))}
-                    </div>
+          <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+            <span>Productos</span>
+            <ChevronDownIcon aria-hidden="true" className="w-5 h-5 ml-2" />
+          </PopoverButton>
+    
+          <PopoverPanel
+            className="absolute z-10 mt-3 w-screen max-w-xs sm:max-w-sm lg:max-w-lg left-1/2 -translate-x-1/2 transform bg-white shadow-2xl ring-1 ring-gray-900/5 rounded-lg "
+          >
+            <div className="p-4">
+              {/* Lista de soluciones */}
+              {solutions.map((item) => (
+                <div
+                  key={item.name}
+                  className="flex items-center gap-4 p-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150"
+                >
+                  {/* <item.icon className="w-6 h-6 text-gray-400" /> */}
+                  <div>
+                    <a href={item.href} className="font-semibold text-gray-900 hover:underline">
+                      {item.name}
+                    </a>
+                    {/* <p className="text-sm text-gray-500">{item.description}</p> */}
+                  </div>
                 </div>
-            </PopoverPanel>
+              ))}
+            </div>
+          </PopoverPanel>
         </Popover>
-    )
+      );
 }
